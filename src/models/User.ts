2a6ca823e2f2fa2d4a14d3dcde14a39d9d87d68f
Attila Poroszkai <poroszkai.attila@gmail.com>
2020-01-22 19:38:41 +0100

    Refactoring Sync

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,8 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
-import { home } from './../index';
 import { Eventing } from './Eventing';
 
-interface UserProps {
+export interface UserProps {
     id?: number;
     name?: string;
     age?: number;
@@ -10,7 +8,7 @@ interface UserProps {
 
 export class User {
     public events: Eventing = new Eventing();
-    
+
     constructor(private data: UserProps) {}
 
     get(propName: string): number | string {
@@ -21,22 +19,5 @@ export class User {
         Object.assign(this.data, update);
     }
 
-    fetch(): void {
-        axios.get(home + "/users/" + this.get('id'))
-            .then((response: AxiosResponse): void => {
-                this.set(response.data);
-            });
-    }
-
-    save(): void {
-        const id = this.get('id');
-
-        if(this.get('id')) {
-            // put
-            axios.put(home + `/users/${this.get('id')}`, this.data);
-        } else {
-            // poset
-            axios.post(home + `/users`, this.data);
-        }
-    }
+   
 }
